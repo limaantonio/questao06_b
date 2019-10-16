3ebfce8255a34f937abce50a589bb2f6d648f809
antonio <limaantoniocarlos20@gmail.com>
2019-10-15 21:04:41 -0300

    Questao 6, letra b corrigida. Metodo de ordenacao alterado

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,108 +7,113 @@ import java.util.Scanner;
 import entities.Funcionario;
 
 public class Program {
-	public static void bubbleSort(Funcionario[] v, int n, int o) {
+	public static void selectionSort(Funcionario[] v, int n, int o) {
 		Funcionario aux = new Funcionario();
-		if(o == 1) {
-			for (int i=0; i<n; i++) {
-				for(int j=0; j<n-i-1; j++) {
-					if(v[j].getSalario() >  v[j+1].getSalario()) {
-						aux = v[j];
-						v[j] = v[j+1];
-						v[j+1] = aux;
+		if (o == 1) {
+			int min;
+			for (int i = 0; i < n - 1; i++) {
+				min = i;
+				for (int j = i + 1; j < n; j++) {
+					if (v[j].getSalario() < v[min].getSalario()) {
+						min = j;
 					}
 				}
+				aux = v[i];
+				v[i] = v[min];
+				v[min] = aux;
 			}
 		}
-		if(o == 2) {
-			for (int i=0; i<n; i++) {
-				for(int j=0; j<n-i-1; j++) {
-					if(v[j].getNome().charAt(0) >  v[j+1].getNome().charAt(0)) {
-						aux = v[j];
-						v[j] = v[j+1];
-						v[j+1] = aux;
+		if (o == 2) {
+			int min;
+			for (int i = 0; i < n - 1; i++) {
+				min = i;
+				for (int j = i + 1; j < n; j++) {
+					if (v[j].getNome().charAt(1) < v[min].getNome().charAt(1)) {
+						min = j;
 					}
 				}
+				aux = v[i];
+				v[i] = v[min];
+				v[min] = aux;
 			}
 		}
 	}
-	
+
 	public static void insertionSort(Funcionario[] v, int n, int o) {
 		Funcionario x = new Funcionario();
-		int j=0;
-		
-		if(o == 1) {
-			for(int i=1; i<n; i++) {
+		int j = 0;
+
+		if (o == 1) {
+			for (int i = 1; i < n; i++) {
 				x = v[i];
-				j = i-1;
-				while(j >=0 && v[j].getSalario() > x.getSalario()) {
-					v[j+1] = v[j];
+				j = i - 1;
+				while (j >= 0 && v[j].getSalario() > x.getSalario()) {
+					v[j + 1] = v[j];
 					j--;
 				}
-				v[j+1] = x;
+				v[j + 1] = x;
 			}
 		}
-		if(o == 2) {
-			for(int i=1; i<n; i++) {
+		if (o == 2) {
+			for (int i = 1; i < n; i++) {
 				x = v[i];
-				j = i-1;
-				while(j >=0 && v[j].getNome().charAt(0) > x.getNome().charAt(0)) {
-					v[j+1] = v[j];
+				j = i - 1;
+				while (j >= 0 && v[j].getNome().charAt(0) > x.getNome().charAt(0)) {
+					v[j + 1] = v[j];
 					j--;
 				}
-				v[j+1] = x;
+				v[j + 1] = x;
 			}
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
-		int n=2;
+
+		int n = 2;
 		Scanner sc = new Scanner(System.in);
 		Funcionario[] v = new Funcionario[n];
 		Funcionario f = new Funcionario();
-		
-		
-		for (int i=0; i<n; i++){
+
+		for (int i = 0; i < n; i++) {
 			System.out.print("Digite o nome do funcionario: ");
 			String nome = sc.next();
 			System.out.print("Digite o salario: ");
 			double salario = sc.nextDouble();
-			
+
 			v[i] = new Funcionario(nome, salario);
 		}
-		
-		for(Funcionario  fun : v) {
-			 fun.toString();
+
+		for (Funcionario fun : v) {
+			fun.toString();
 		}
-		
-		System.out.println(".......Ordenação BubbleSort........");
-		bubbleSort(v, n, 1);
+
+		System.out.println(".......Selection BubbleSort........");
+		selectionSort(v, n, 1);
 		System.out.println("Funcionarios por ordem crescente de salario:");
-		for(Funcionario  fun : v) {
-			 System.out.println(fun.toString());
+		for (Funcionario fun : v) {
+			System.out.println(fun.toString());
 		}
-		bubbleSort(v, n, 2);
+		selectionSort(v, n, 2);
 		System.out.println();
 		System.out.println("Funcionarios com nomes em ordem crescente:");
-		for(Funcionario  fun : v) {
-			 System.out.println(fun.toString());
+		for (Funcionario fun : v) {
+			System.out.println(fun.toString());
 		}
-		
+
 		System.out.println();
-		
+
 		System.out.println(".......Ordenação InsertionSort........");
 		insertionSort(v, n, 1);
 		System.out.println("Funcionarios por ordem crescente de salario:");
-		for(Funcionario  fun : v) {
-			 System.out.println(fun.toString());
+		for (Funcionario fun : v) {
+			System.out.println(fun.toString());
 		}
 		System.out.println();
 		insertionSort(v, n, 2);
 		System.out.println("Funcionarios com nomes em ordem crescente:");
-		for(Funcionario  fun : v) {
-			 System.out.println(fun.toString());
-		}	
+		for (Funcionario fun : v) {
+			System.out.println(fun.toString());
+		}
 	}
 }
